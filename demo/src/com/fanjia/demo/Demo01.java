31673f37962f554de437f5294d2f4ea99ab3ba22
fjdaima123 <1580332947@qq.com>
2020-09-28 14:51:45 +0800

    Initial commit one push

---
+++ demo/src/com/fanjia/demo/Demo01.java
@@ -0,0 +1,16 @@
+package com.fanjia.demo;
+
+import javax.sql.DataSource;
+
+/**
+ * Created by FanJia on 2020/9/28 9:19
+ */
+public class Demo01 {
+    public static void main(String[] args) {
+        Integer n1 = 100;
+        Integer n2 = 100;
+        System.out.println(n1==n2);
+        DataSource dataSource;
+        System.out.println("你好！哈喽，github,");
+    }
+}
